86af2e6b13db8d3a30ca064e7b88fe1fb2cb7e63
hareeqi <hareeqi@gmail.com>
2017-04-02 19:46:16 +0200

    basic auth

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//server
+const app = require('express')()
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const config = require('./config.json')
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require('./routes'))
+
+
+app.listen(config.bind_port, config.bind_address,()=>
+console.log("iot-bgw-auth listening on %s:%d ",config.bind_address,  config.bind_port));
--- routes.js
+++ routes.js
@@ -1,1 +1,7 @@
-//routes
+const routes = require('express')()
+
+routes.post('/validate',(req,res)=>{
+  res.status(200).json({allowed:true})
+})
+
+module.exports = routes
