d47db6c1c196a89bdbb7103aa40355581f1a3273
hareeqi <hareeqi@gmail.com>
2017-04-25 19:07:52 +0200

    auth key works

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req,res,next)=>{
 
-    if(req.headers.authorization == config.bgw_admin_key_string){
+    if(req.headers.authorization == config.bgw_admin_key){
       next()
     } else {
       res.status(403).json({status:false, error:'Invalid border gateway admin key' })
