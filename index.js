66bf3d3352ce45e269d2f25b6d778179f38c475a
hareeqi <hareeqi@gmail.com>
2017-08-28 18:58:20 +0200

    remove ei default

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const cors = require('cors');
 const config = require('./config')
 const {AAA, CAT} = require('../iot-bgw-aaa-client')
 
+const auth_provider = process.env.AAA_CLIENT_AUTH_PROVIDER
+if(auth_provider!="internal"){
+  console.log("The BGW uses",auth_provider,"and does not use the internal auth provider, shutting down...  ");
+  process.exit(0);
+}
+
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
